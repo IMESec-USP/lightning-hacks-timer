c23d1539be609f0b987354576f7f4d183a1e55d0
pedro823 <grey.pedrinho@gmail.com>
2019-04-18 01:12:15 -0300

    Initial commit of clock

--- src/App.js
+++ src/App.js
@@ -1,28 +1,173 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+
+/*
+  Credits where it's due, some code was inspired by VTEX's timer app,
+  at github.com/vtex/show-time
+*/
+
+const twoDigits = (time) => ('0' + time).slice(-2)
 
 class App extends Component {
-  render() {
+  constructor() {
+    super()
+    this.state = {
+      minutes: 5,
+      seconds: 0,
+      minutesAtStart: 5,
+      running: null,
+      timeEnded: false,
+      loading: true,
+      showingHelp: false,
+    }
+  }
+
+  handleKeyPress = (event) => {
+    const { running } = this.state
+    switch (event.key) {
+      case ' ':
+      case 'Enter':
+        running ? this.pauseTimer() : this.startTimer()
+        break
+      case 'ArrowUp':
+        this.incrementMinute()
+        break
+      case 'ArrowDown':
+        this.decrementMinute()
+        break
+      case 'r':
+      case 'R':
+        this.resetState()
+        break
+      case 'H':
+      case 'h':
+      case '?':
+        this.toggleHelp()
+        break
+      default:
+        break
+    }
+  }
+
+  toggleHelp() {
+    this.setState({ showingHelp: !this.state.showingHelp })
+  }
+
+  incrementMinute() {
+    const { minutes, running, minutesAtStart, timeEnded } = this.state
+    if (timeEnded) return
+    if (minutes < 99) {
+      const newMinutesAtStart = running ? minutesAtStart : minutes + 1
+      this.setState({
+        minutes: minutes + 1,
+        minutesAtStart: newMinutesAtStart,
+      })
+    }
+  }
+
+  decrementMinute() {
+    const { running, minutes, minutesAtStart, seconds, timeEnded } = this.state
+    if (timeEnded) return
+    if (minutes > 1 || (minutes > 0 && seconds > 0)) {
+      const newMinutesAtStart = running ? minutesAtStart : minutes - 1
+      this.setState({
+        minutes: minutes - 1,
+        minutesAtStart: newMinutesAtStart,
+      })
+    }
+  }
+
+  startTimer() {
+    const { running } = this.state
+    if (running) return
+    const runningInterval = setInterval(() => {
+      this.setState(previousState => {
+        const { timeEnded, minutes, seconds } = previousState
+        if (timeEnded) {
+          return {
+            ...previousState,
+            ...this.incrementTime(minutes, seconds),
+          }
+        }
+        if (seconds === 0 && minutes === 0) {
+          return {
+            ...previousState,
+            timeEnded: true,
+            seconds: 1,
+          }
+        }
+        return {
+          ...previousState,
+          ...this.decrementTime(minutes, seconds),
+        }
+      })
+    }, 1000)
+    this.setState({
+      running: runningInterval,
+    })
+  }
+
+  pauseTimer() {
+    const { running } = this.state
+    clearInterval(running)
+    this.setState({ running: null })
+  }
+
+  incrementTime(minutes, seconds) {
+    const newSeconds = seconds === 59 ? 0 : seconds + 1
+    const newMinutes = seconds === 59 ? minutes + 1 : minutes
+    return {
+      minutes: newMinutes,
+      seconds: newSeconds,
+    }
+  }
+
+  decrementTime(minutes, seconds) {
+    const newSeconds = seconds === 0 ? 59 : seconds - 1
+    const newMinutes = seconds === 0 ? minutes - 1 : minutes
+    return {
+      minutes: newMinutes,
+      seconds: newSeconds,
+    }
+  }
+
+  resetState() {
+    this.pauseTimer()
+    this.setState({
+      minutes: this.state.minutesAtStart,
+      seconds: 0,
+      running: false,
+      timeEnded: false,
+    })
+  }
+
+  componentDidMount() {
+    this.setState({ loading: false }, () => {
+      document && document.addEventListener('keyup', this.handleKeyPress)
+    })
+  }
+
+  showHelp() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="help white">
+        <p>
+          HelpText
+        </p>
       </div>
-    );
+    )
+  }
+
+  render() {
+    const { showingHelp, timeEnded, minutes, seconds } = this.state
+    return (
+      <div className="timer">
+        <p className="timer--counter white">
+          { timeEnded && '-' }{ twoDigits(minutes) }:{ twoDigits(seconds) }
+        </p>
+        { showingHelp && this.showHelp() }
+      </div>
+    )
   }
 }
 
-export default App;
+export default App
