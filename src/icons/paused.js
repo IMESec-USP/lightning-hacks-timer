7340a834945c71fdba4442826ab4297204f4c31c
pedro823 <grey.pedrinho@gmail.com>
2019-04-21 01:42:40 -0300

    Makes play/pause buttons touch

--- src/icons/paused.js
+++ src/icons/paused.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import PointTarget from 'react-point'
 
 export default (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" className={'icon ' + (props.highlighted ? 'fill-white' : 'fill-grey') } viewBox="0 0 20 20">
-    <rect width="6" height="16" x="3" y="2" rx="1" ry="1"/>
-    <rect width="6" height="16" x="11" y="2" rx="1" ry="1"/>
-  </svg>
+  <PointTarget onPoint={props.onPress}>
+    <svg xmlns="http://www.w3.org/2000/svg" 
+        className={'icon ' + (props.highlighted ? 'fill-white' : 'fill-grey') } 
+        viewBox="0 0 20 20"
+    >
+      <rect width="6" height="16" x="3" y="2" rx="1" ry="1"/>
+      <rect width="6" height="16" x="11" y="2" rx="1" ry="1"/>
+    </svg>
+  </PointTarget>
 )
--- src/icons/play.js
+++ src/icons/play.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import PointTarget from 'react-point'
 
 export default (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" className={'icon ' + (props.highlighted ? 'fill-white' : 'fill-grey')} viewBox="0 0 1024 1024" version="1.1">
-      <path d="M103.817525 103.55014c0-95.810065 65.879405-131.687801 146.386843-79.690825l609.566461 393.589947c80.542943 51.979224 80.542943 137.066798 0 189.046022l-609.566461 393.660957c-80.507438 51.979224-146.386843 16.101488-146.386843-79.708577L103.817525 103.55014z"/>
-  </svg>
+  <PointTarget onPoint={props.onPress}>
+    <svg xmlns="http://www.w3.org/2000/svg" 
+        className={'icon ' + (props.highlighted ? 'fill-white' : 'fill-grey')} 
+        viewBox="0 0 1024 1024" 
+        version="1.1"
+    >
+        <path d="M103.817525 103.55014c0-95.810065 65.879405-131.687801 146.386843-79.690825l609.566461 393.589947c80.542943 51.979224 80.542943 137.066798 0 189.046022l-609.566461 393.660957c-80.507438 51.979224-146.386843 16.101488-146.386843-79.708577L103.817525 103.55014z"/>
+    </svg>
+  </PointTarget>
 )
